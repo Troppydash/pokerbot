1a3733f2fa68992c98e0c3406e59a29f73a360a6
Troppydash <troppydash.developer@gmail.com>
2025-12-06 17:45:30 +1300

    Added filecache comments

--- Bots/Shared/FileCache.cs
+++ Bots/Shared/FileCache.cs
@@ -6,6 +6,13 @@ namespace PokerBot.Bots.Shared;
 [Stable]
 public class FileCache
 {
+    /// <summary>
+    /// Caches a computation in a disk file
+    /// </summary>
+    /// <param name="path">Disk file name</param>
+    /// <param name="compute">Lambda computation</param>
+    /// <typeparam name="T">Type of data to store</typeparam>
+    /// <returns>Data either loaded from disk, or computed and saved using lambda</returns>
     public static T OptionalCompute<T>(string path, Func<T> compute)
     {
         if (File.Exists(path))
