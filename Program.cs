f61a9cc1e533716d2b02514ff9210977d04b91f2
Troppydash <troppydash.developer@gmail.com>
2025-12-06 17:13:52 +1300

    added readme

--- Program.cs
+++ Program.cs
@@ -9,23 +9,41 @@ public class Program
     {
         int[] scores = [0, 0];
 
+        Random rng = new Random(42);
+        List<double> winnings = [];
         for (int i = 0; i < iters; ++i)
         {
             Console.WriteLine($"Game {i}");
-            int seed = Random.Shared.Next();
+            int seed = rng.Next();
 
             Arena arena = new Arena([agents[0], agents[1]]);
             int[] result = arena.Simulate(seed, verbose);
             scores[0] += result[0];
             scores[1] += result[1];
-
+            winnings.Add(result[0]);
 
             arena = new Arena([agents[1], agents[0]]);
             result = arena.Simulate(seed, verbose);
             scores[0] += result[1];
             scores[1] += result[0];
+            winnings.Add(result[1]);
 
-            Console.WriteLine($"{scores[0] / (i+1) / 2}, {scores[1]  / (i+1) / 2}");
+            double avg = 0.0;
+            foreach (var win in winnings)
+            {
+                avg += win;
+            }
+            avg /= winnings.Count;
+
+            double std = 0.0;
+            foreach (var win in winnings)
+            {
+                std += (win - avg) * (win - avg);
+            }
+
+            std = Math.Sqrt(std / winnings.Count);
+            double se = std / Math.Sqrt(winnings.Count);
+            Console.WriteLine($"Player 0 {avg:0.00}, CI 95% = ({avg - 1.96 * se:0.00}, {avg + 1.96 * se:0.00})");
         }
 
 
@@ -34,23 +52,32 @@ public class Program
 
     static void Solve()
     {
-        Infoset set = Infoset.FromClusters(false);
+        Infoset set = Infoset.FromClusters();
         Solver solver = new Solver(set);
-        solver.Simulate(1000000, 40);
+        solver.Simulate(1000000, 56453);
     }
 
     public static void Main()
     {
+        // foreach (var comb in Helper.LazyCombinations(5, 3))
+        // {
+        //     Console.WriteLine($"{comb[0]} {comb[1]} {comb[2]}");
+        // }
+        // FastResolver.Cached.GetInstance();
+        // EmdCluster.MakePrivateClusterPoints(0);
         // TODO: simulate bot
-        Solve();
+        // Solve();
 
-        // Infoset set = Infoset.FromClusters(false);
-        // CfrAgent agent = new CfrAgent(set, Solver.Result.Load("result3.json", set));
+        // Infoset set = Infoset.FromClusters();
+        // CfrAgent agent = new CfrAgent(set, Solver.Result.Load("result4.json", set));
         // int n = 100000;
-        // var result = SimulateAll([new EvAgent(false), agent], n, true);
-        //
-        //
+
+        // var result = SimulateAll([new EvAgent(false), new ManualAgent()], 1000, false);
+        var result = SimulateAll([new EvAgent(false), new ManualAgent()], 1000, true);
         
+        //
+        //
+
         // Console.WriteLine($"player 0 average utility {(float)result[0] / (2*n)}, player 1 average utility {(float)result[1] / (2*n)}");
         // Card[] cards = Card.AllCards();
         // var dist = EMDCluster.ComputeEquityDistribution(
--- README.md
+++ README.md
@@ -1,3 +1,30 @@
-﻿# Resources and how to
+﻿# Poker Bot
 
-Less pain for future me, bruh
+## Build and Run
+1. Download Jetbrains rider
+2. Click run
+3. Profit?
+
+## Structure
+The core components are
+- `Program.cs` is the entry point used to debug, try not to commit it
+- `Game.cs` contains the Texas hold 'em no limit implementation, key classes are
+  - `Card` for a poker card
+  - `Action` for a player action during play
+  - `HandResolver` to compare final hands
+  - `Game` for the poker game
+  - `Game.State` for a complete, one-sided description of the game
+- `Agent.cs` provides the interface of a poker playing bot
+- `Arena.cs` provides the implementation of a 1v1 poker game tester using `IAgent`s
+
+The `Bots` folder contains bot specific code, the `Bots/Shared` folder contains common components
+- `AbstractGame.cs` for a pot proportion variant of poker with fewer abstract actions
+- `FastResolver.cs` for a faster version of hand comparisons
+- `FileCache.cs` for disk memo computations
+- `Helper.cs` for misc deck sampling algorithms methods
+
+## Bots
+- `RandomAgent.cs` exports random naive bots
+- `EVAgent.cs` for a no-tree-search, equity heuristic bot
+- `Cfr` for an attempted cfr poker player (not working)
+- `ExpectedMinimax` for the Expectiminimax implementation
